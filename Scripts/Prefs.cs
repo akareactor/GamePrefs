98c7fc9bc2a9d56b319a81057c3c88a620b5b052
akareactor <akareactor@mail.ru>
2025-10-08 21:49:47 +0300

    Removed erratic using

--- Scripts/Prefs.cs
+++ Scripts/Prefs.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.IO;
-using UnityEngine.InputSystem.Interactions;
 
 // просто чтобы легче было контролировать вызовы обёртки
 public static class Prefs {
